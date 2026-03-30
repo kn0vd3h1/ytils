f508018273d17c4907bcd65c2598a141ab5c6ad4
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-03-30 20:33:47 +0000

    Update exploit based on attempt 4 feedback

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * configuration: https://github.com/okonet/lint-staged#Configuration
- */
-module.exports = {
-    "*.{js,ts}": ["eslint --fix", "prettier --write"],
-    "*.{json,jsonc}": ["prettier --write"],
-    "*.{yml,yaml}": ["prettier --write"],
-    "*.{md,markdown}": ["markdownlint-cli2", "prettier --write"],
-};
